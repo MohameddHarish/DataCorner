1519fbbb18b8c523910a669ed5e656f478fd4555
MohamedHarishh <mohamed@sightspectrum.in>
2023-08-02 14:36:19 +0530

    Dropdown model

---
+++ DataCorner/Models/ProjectIDDropdown.cs
@@ -0,0 +1,8 @@
+﻿namespace DataCorner.Models
+{
+    public class ProjectIDDropdown : IDropdownOption
+    {
+        public string Project_id { get; set; }
+        public string DisplayText => Project_id;
+    }
+}
