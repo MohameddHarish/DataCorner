4471372c121137257bbf3e4aed8797ea08804641
MohamedHarishh <mohamed@sightspectrum.in>
2023-08-03 10:17:56 +0530

    login api done

---
+++ DataCorner/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace DataCorner.Models
+{
+    public class Login
+    {
+        //public string? Flag { get; set; }   
+        public string? UserName { get; set; }
+        public string? Pwd { get; set; }
+        public string? IsAdmin { get; set; }
+    }
+}
