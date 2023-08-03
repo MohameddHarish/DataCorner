16e3af827797c25b03ab23daea19b6f624da76a8
Karthikeyan <karthikeyan@sightspectrum.com>
2023-08-03 10:26:08 +0530

    Environment ts file created

--- DataCorner/TD/src/app/component/login/login.component.ts
+++ DataCorner/TD/src/app/component/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../service/authentication.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,8 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private http: HttpClient
   ) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
@@ -32,11 +34,26 @@ export class LoginComponent implements OnInit {
       const enteredUsername = this.loginForm.value.username;
       const enteredPassword = this.loginForm.value.password;
 
+    //   const username = this.loginForm.value.username;
+    // const password = this.loginForm.value.password;
+
+    // const loginData = {
+    //   username: username,
+    //   password: password,
+    // };
+
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    //   // You may add other headers like authorization token here if needed
+    // });
+
       this.showSpinner = true; // Step 3: Show the spinner when the login button is clicked
 
       setTimeout(() => { // Simulate an asynchronous login process (you should use your actual login process)
 
       const roleId = this.authService.validateCredentials(enteredUsername, enteredPassword);
+
+      
 
 
       if (roleId !== null) {
@@ -52,6 +69,17 @@ export class LoginComponent implements OnInit {
     }, 2000); // Simulating a 2-second delay for the login process
 
     console.log("test2");
+
+    // this.http.post('backend-url/login', loginData, { headers }).subscribe(
+    //   (response) => {
+    //     // Handle the response from the backend (e.g., success message or user data)
+    //     console.log('Login successful:', response);
+    //   },
+    //   (error) => {
+    //     // Handle errors, if any, returned from the backend
+    //     console.error('Login failed:', error);
+    //   }
+    // );
 
     }
   }
