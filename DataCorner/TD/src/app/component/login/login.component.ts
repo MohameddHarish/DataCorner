a79a126b60366c32e1c052867b6a4fb4ef58f3d7
MohamedHarishh <mohamed@sightspectrum.in>
2023-08-07 12:34:11 +0530

    snack bar message set to top of the page

--- DataCorner/TD/src/app/component/login/login.component.ts
+++ DataCorner/TD/src/app/component/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../service/authentication.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -43,17 +43,26 @@ export class LoginComponent implements OnInit {
             if (response.success) {
               const roleId = response.data.isAdmin === 'Admin' ? 1 : 0;
               const msg = roleId === 1 ? 'admin' : 'user';
-              this.snackBar.open(`Login successful as ${msg}.`, 'Close', { duration: 3000 });
+              this.snackBar.open(`Login successful as ${msg}.`, 'Close', {
+                duration: 3000,
+                verticalPosition: 'top' 
+              });
               this.authService.setCurrentUser({ username: enteredUsername, roleId: roleId });
               this.router.navigate(['dashboard', { roleId: roleId }]); // Use roleId here
             } else {
-              this.snackBar.open('Invalid username or password.', 'Close', { duration: 3000 });
+              this.snackBar.open('Invalid username or password.', 'Close', {
+                duration: 3000,
+                verticalPosition: 'top'
+              });
             }
             this.showSpinner = false;
           },
           (error) => {
             console.error('Login failed:', error);
-            this.snackBar.open('An error occurred during login.', 'Close', { duration: 3000 });
+            this.snackBar.open('An error occurred during login.', 'Close', {
+              duration: 3000,
+              verticalPosition: 'top' 
+            });
             this.showSpinner = false;
           }
         );
