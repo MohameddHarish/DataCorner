0308395fece64e218b55789a1c13848c244482dc
MohamedHarishh <mohamed@sightspectrum.in>
2023-09-04 15:42:46 +0530

    Enhancement(Employeetable and manage component): Create a checkboxes in 1st column in table if checked rows only updated to data base..

--- DataCorner/TD/src/app/component/manage-permission/manage-permission.component.ts
+++ DataCorner/TD/src/app/component/manage-permission/manage-permission.component.ts
@@ -77,7 +77,8 @@ export class ManagePermissionComponent implements OnInit {
   
     if (event.checked) {
       // Add the column to the selected columns for the role
-      selectedColumns.push(column);
+      //selectedColumns.push(column);
+      selectedColumns.splice(-2, 0,column);
     } else {
       // Remove the column from the selected columns for the role
       const columnIndex = selectedColumns.indexOf(column);
